26172d253cd2437fba5ee1aaa6376a6475748f7d
Lubos Jones <l.furinda@gmail.com>
2020-02-04 16:50:52 +0100

    Basic components added

---
+++ src/components/content-container/content-container.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import './content-container.styles.scss';
+
+const ContentContainer = () => (
+    <div className="content-wrapper">
+        <div className="block">ONE</div>
+        <div className="block">TWO</div>
+    </div>
+);  
+
+export default ContentContainer;
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './header.styles.scss';
+
+const Header = () => (
+    <div className="header">
+        <div className="cart-icon">
+            <i class="fas fa-shopping-cart"></i>
+            <p className="text">Shopping Cart App</p>
+        </div>
+    </div>
+)
+
+export default Header;

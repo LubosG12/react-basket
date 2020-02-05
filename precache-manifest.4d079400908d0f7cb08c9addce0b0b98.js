1775163bb5bb6dbd528ccf0a8d7b680636ac47a2
Lubos Jones <l.furinda@gmail.com>
2020-02-05 15:55:03 +0100

    Updates

---
+++ precache-manifest.4d079400908d0f7cb08c9addce0b0b98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32385d64c2f3165621c9ffa4ecc7ea45",
+    "url": "/react-basket/index.html"
+  },
+  {
+    "revision": "c6c3335bd0545446c4f0",
+    "url": "/react-basket/static/css/main.2c5042a8.chunk.css"
+  },
+  {
+    "revision": "5de9ee801d103be981ef",
+    "url": "/react-basket/static/js/2.2d91ec09.chunk.js"
+  },
+  {
+    "revision": "94b03e19a4c9eb66db847f3cca1e03ba",
+    "url": "/react-basket/static/js/2.2d91ec09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c3335bd0545446c4f0",
+    "url": "/react-basket/static/js/main.07c109df.chunk.js"
+  },
+  {
+    "revision": "a8810f01d8ab9fb71e7a",
+    "url": "/react-basket/static/js/runtime-main.902fed8e.js"
+  }
+]);
